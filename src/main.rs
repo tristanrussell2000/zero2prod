ad962f4f29aa68aceef519249d07e7095c1c0fef
Tristan Russell <tristanrussell2000@gmail.com>
2026-03-04 19:58:17 -0800

    Fix?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use sqlx::postgres::PgPoolOptions;
-use std::time::Duration;
 use tokio::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,7 +11,6 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
     let address = format!("127.0.0.1:{}", configuration.application.port);
     let listener = TcpListener::bind(address).await?;
