1d9bec781abe14dc3f46b0e7265f143bda5f0ea0
Tristan Russell <tristanrussell2000@gmail.com>
2026-03-04 23:21:47 -0800

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,11 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.with_db());
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).await?;
     run(listener, connection_pool)?.await
 }
