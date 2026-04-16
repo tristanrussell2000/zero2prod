2046ed2c395ccff845ef9f358a5ad78a7e77b51c
Tristan Russell <tristanrussell2000@gmail.com>
2026-04-15 23:22:43 -0700

    Formatting

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
-use axum::http::{HeaderValue, StatusCode};
 use axum::http::header::WWW_AUTHENTICATE;
+use axum::http::{HeaderValue, StatusCode};
 use axum::response::{IntoResponse, Response};
 
 fn error_chain_fmt(
@@ -27,12 +27,22 @@ pub enum AppError {
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let status = match self {
-            AppError::ValidationError(_) => Response::builder().status(StatusCode::BAD_REQUEST).body(()).unwrap(),
+            AppError::ValidationError(_) => Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(())
+                .unwrap(),
             AppError::AuthError(_) => {
                 let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
-                Response::builder().status(StatusCode::UNAUTHORIZED).header(WWW_AUTHENTICATE, header_value).body(()).unwrap()
-            },
-            _ => Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(()).unwrap(),
+                Response::builder()
+                    .status(StatusCode::UNAUTHORIZED)
+                    .header(WWW_AUTHENTICATE, header_value)
+                    .body(())
+                    .unwrap()
+            }
+            _ => Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(())
+                .unwrap(),
         };
 
         tracing::error!(exception.message = %self, exception.details = ?self, "Response failed");
