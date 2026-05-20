263037163d0fe867ff81e468e16feb18ba6fc7eb
Tristan Russell <tristanrussell2000@gmail.com>
2026-05-19 20:46:14 -0700

    Use password hash instead

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -8,6 +8,7 @@ use axum::extract::rejection::JsonRejection;
 use axum::http::StatusCode;
 use reqwest::header::HeaderMap;
 use secrecy::{ExposeSecret, SecretString};
+use sha3::Digest;
 use sqlx::PgPool;
 use std::sync::Arc;
 
@@ -106,10 +107,12 @@ async fn validate_credentials(
     credentials: &Credentials,
     pg_pool: &PgPool,
 ) -> Result<uuid::Uuid, anyhow::Error> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
-        r#"SELECT user_id FROM users WHERE username = $1 AND password = $2"#,
+        r#"SELECT user_id FROM users WHERE username = $1 AND password_hash = $2"#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pg_pool)
     .await
